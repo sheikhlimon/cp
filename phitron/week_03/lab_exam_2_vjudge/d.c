69ff6f29e7d9edccdce9e411ccc0e749870d98f4
Sheikh Limon <sheikhlimon404@gmail.com>
2022-05-02 12:32:33 +0600

    initial commit

---
+++ phitron/week_03/lab_exam_2_vjudge/d.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char str[100];
+    scanf("%s", &str);
+    int a, d = 0;
+    for (int i = 0; i < n; i++)
+        if (str[i] == 'A')
+            a++;
+        else
+            d++;
+    if (a > d)
+        printf("Anton");
+    if (a < d)
+        printf("Danik");
+    if (a == d)
+        printf("Friendship");
+    return 0;
+}
