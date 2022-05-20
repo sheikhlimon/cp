617955395d5627c65a1c76ed2b6ced88dce67765
Sheikh Limon <sheikhlimon404@gmail.com>
2022-05-20 22:35:21 +0600

    initial commit

---
+++ phitron/week_05/exam_4/6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+void makeArray(int n, int squares[])
+{
+    for(int i =1; i<=n; i++)
+    {
+        printf("%d ", i*i);
+    }
+}
+
+int main()
+{
+    int squares[100];
+    makeArray(4, squares);
+}
