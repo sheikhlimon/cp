498da5d8275a73e402326b3451e4d5b54af0d146
Sheikh Limon <sheikhlimon404@gmail.com>
2022-05-28 17:55:37 +0600

    initial commit

--- phitron/week_06/exam_5/7.c
+++ phitron/week_06/exam_5/7.c
@@ -2,28 +2,10 @@
 #include <string.h>
 #include <stdbool.h>
 
-void sort(int n, int a[])
-{
-    for (int steps = 0; steps < n; steps++)
-    {
-        for (int i = n - 1; i > 0; i--)
-        {
-            if (a[i] < a[i - 1])
-            {
-                int temp = a[i];
-                a[i] = a[i - 1];
-                a[i - 1] = temp;
-            }
-        }
-    }
-}
-
 bool is_reverse_sorted(int n, int a[])
 {
-    if (n == 0 || n == 1)
-        return true;
-    for (int i = 1; i < n; i++)
-        if (a[i] < a[i - 1])
+    for (int i = 0; i < n - 1; i++)
+        if (a[i] < a[i + 1])
             return false;
     return true;
 }
@@ -31,9 +13,8 @@ bool is_reverse_sorted(int n, int a[])
 int main()
 {
     int n = 4;
-    int a[] = {6, 4, 3, 1};
-    sort(n, a);
-    if(is_reverse_sorted(n, a))
+    int a[] = {6, 8, 3, 1};
+    if (is_reverse_sorted(n, a))
         printf("y");
     else
         printf("n");
