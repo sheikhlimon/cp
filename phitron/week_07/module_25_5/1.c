f35f4dd67dc58fbd7dab143b4dcea44db1abafb2
Sheikh Limon <sheikhlimon404@gmail.com>
2022-05-31 19:50:06 +0600

    initial commit

---
+++ phitron/week_07/module_25_5/1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Date {
+    int day; 
+    int month;
+    int year;
+};
+
+struct Student {
+    char *name[100];
+    int class;
+    int roll;
+    struct Date dob;
+};
+
+
+int main()
+{
+    struct Student s1 = {"Peter Parker", 4, 23};
+
+    Student.name = "Peter Parker";
+
+
+    return 0;
+}
