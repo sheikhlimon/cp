adb8827ea4fc9728852a2fd26b65297e988a5f54
Sheikh Limon <sheikhlimon404@gmail.com>
2022-06-01 20:33:21 +0600

    initial commit

---
+++ c/pointers/derefencing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int a = 1;
+
+    int *pointer_to_a = &a;
+
+    a += 1;
+
+    *pointer_to_a += 1; 
+
+    printf("The value of a is %d\n", a);
+
+    return 0;
+}
--- phitron/week_07/module_25_5/1.c
+++ phitron/week_07/module_25_5/1.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Date {
-    int day; 
+struct Date
+{
+    int day;
     int month;
     int year;
 };
 
-struct Student {
+struct Student
+{
     char *name[100];
     int class;
     int roll;
     struct Date dob;
 };
 
-
 int main()
 {
-    struct Student s1 = {"Peter Parker", 4, 23};
+    struct Student s1;
+    s1.name[0] = "Peter Paker";
+    s1.class = 4;
+    s1.roll = 23;
+    
 
-    Student.name = "Peter Parker";
-
+    printf("%s %d %d", s1.name[0], s1.class, s1.roll);
 
     return 0;
 }
