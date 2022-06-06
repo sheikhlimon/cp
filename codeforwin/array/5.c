ad5df62b106e5a665bfb8650a0adf8b84a3825c6
Sheikh Limon <sheikhlimon404@gmail.com>
2022-06-06 12:32:01 +0600

    initial commit

---
+++ codeforwin/array/5.c
@@ -0,0 +1,16 @@
+/**
+*	author: sheikhlimon
+*	created: 2022.06.04
+**/
+
+#include<stdio.h>
+#include<string.h>
+
+
+
+int main()
+{
+       
+
+    return 0;
+}
---
+++ codeforwin/function/prime.c
@@ -0,0 +1,36 @@
+/**
+ *	author: sheikhlimon
+ *	created: 2022.06.04
+ **/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i, num, isPrime;
+
+    isPrime = 1;
+
+    printf("enter number: ");
+    scanf("%d", &num);
+
+    for (i = 2; i <= num / 2; i++)
+    {
+        if (num % i == 0)
+        {
+            isPrime = 0;
+            break;
+        }
+    }
+
+    if (isPrime == 1 && num > 1)
+    {
+        printf("%d is prime number", num);
+    }
+    else
+    {
+        printf("%d is composite number", num);
+    }
+    return 0;
+}
