a135d362bb4adea69ed36d2fc011cbfb7ac23d36
Sheikh Limon <sheikhlimon404@gmail.com>
2022-06-08 17:13:48 +0600

    initial commit

--- codeforwin/array/5.c
+++ codeforwin/array/5.c
@@ -1,16 +1,42 @@
 /**
-*	author: sheikhlimon
-*	created: 2022.06.04
-**/
+ *	author: sheikhlimon
+ *	created: 2022.06.04
+ **/
 
-#include<stdio.h>
-#include<string.h>
-
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
-       
+    int n;
+    scanf("%d", &n);
 
+    int arr[100];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int max1, max2;
+
+    max1 = max2 = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max1)
+        {
+            max2 = max1;
+            max1 = arr[i];
+            
+        }
+        else if (arr[i] > max2 && arr[i] < max1)
+        {
+            max2 = arr[i];
+        }
+    }
+
+    printf("First largest = %d\n", max1);
+    printf("Second largest = %d\n", max2);
     return 0;
 }
