30c66d4d8e33b24b401b3dcaa8db8837251bd18f
Sheikh Limon <sheikhlimon404@gmail.com>
2022-06-10 02:07:49 +0600

    initial commit

--- codeforwin/array/11.c
+++ codeforwin/array/11.c
@@ -1,11 +1,10 @@
 /**
-*	author: sheikhlimon
-*	created: 2022.06.10
-**/
+ *	author: sheikhlimon
+ *	created: 2022.06.10
+ **/
 
-#include<stdio.h>
-#include<string.h>
-
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
@@ -16,34 +15,34 @@ int main()
     scanf("%d", &size);
 
     printf("enter elements in array: ");
-    for(i=0;i<size; i++)
+    for (i = 0; i < size; i++)
     {
         scanf("%d", &arr[i]);
         freq[i] = -1;
     }
 
-    for(i=0; i<size; i++)
+    for (i = 0; i < size; i++)
     {
-        count =1; 
-        for(j=0; j<size; j++)
+        count = 1;
+        for (j = 0; j < size; j++)
         {
-            if(arr[i] == arr[j])
+            if (arr[i] == arr[j])
             {
                 count++;
-                freq[j] = 0; 
+                freq[j] = 0;
             }
         }
 
-        if(freq[i] != 0)
+        if (freq[i] != 0)
         {
             freq[i] = count;
         }
     }
 
     printf("\nunique elements in the array are: ");
-    for(i=0; i<size; i++)
+    for (i = 0; i < size; i++)
     {
-        if(freq[i] == 1)
+        if (freq[i] == 1)
         {
             printf("%d ", arr[i]);
         }
