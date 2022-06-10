2ea17230c7151a5dc532f5abfbe656b23863f977
Sheikh Limon <sheikhlimon404@gmail.com>
2022-06-11 01:23:12 +0600

    initial commit

--- phitron/week_08/exam_7/e.c
+++ phitron/week_08/exam_7/e.c
@@ -6,21 +6,16 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main()
-
 {
-
     int n, sum;
-
     scanf("%d", &n);
-
     while (n != 0)
     {
         sum += n % 10;
         n = n / 10;
     }
-
     printf("%d", sum);
-
     return 0;
 }
