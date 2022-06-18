3ab06552a83f9ff0eeab8ff62338ece0fa22ff37
Sheikh Limon <sheikhlimon404@gmail.com>
2022-06-18 18:21:41 +0600

    initial commit

---
+++ phitron/week_09/exam_8/5.c
@@ -0,0 +1,28 @@
+/**
+*	author: sheikhlimon
+*	created: 18.06.2022
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool check_array (int *arr, int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(j=i+1; j<n; j++)
+        {
+            temp = arr[i];
+            arr[i] = arr
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5}
+    check_array(arr, 5)
+
+    return 0;
+}
