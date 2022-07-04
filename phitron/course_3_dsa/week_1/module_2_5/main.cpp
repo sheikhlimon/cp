616d355ebc0146cff3cd5777337d0c21556c2fd4
Sheikh Limon <sheikhlimon404@gmail.com>
2022-07-05 00:45:12 +0600

    initial commit

---
+++ phitron/course_3_dsa/week_1/module_2_5/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a;
+    double b;
+    char c;
+    cin >> a >> b >> c;
+    cout << a << " " << b << " " << c;
+
+    return 0;
+}
