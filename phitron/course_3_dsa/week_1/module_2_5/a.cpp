d86ea7402013476bd84ef656fba0db3d3e4028a9
Sheikh Limon <sheikhlimon404@gmail.com>
2022-07-05 01:46:43 +0600

    initial commit

--- phitron/course_3_dsa/week_1/module_2_5/a.cpp
+++ phitron/course_3_dsa/week_1/module_2_5/a.cpp
@@ -7,11 +7,10 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int a;
-    double b;
-    char c;
-    cin >> a >> b >> c;
-    cout << a << " " << b << " " << c;
+    int a, b, h;
+    cin>>a>>b>>h;
+
+    cout<<(a+b)* h/2<<"\n";
 
     return 0;
 }
