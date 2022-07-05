e60c18de48987236a8d3e7d15cafdcfc8ed33751
Sheikh Limon <sheikhlimon404@gmail.com>
2022-07-05 21:34:18 +0600

    initial commit

---
+++ phitron/course_3_dsa/week_1/module_2_5/test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while(t--)
+    {
+        string s;
+        getline(cin, s);
+        cout << s << endl;
+    }
+
+    return 0;
+}
