9aefda90207e64ae4df83469eb8b12c754bad8e2
sheikhlimon <sheikhlimon404@gmail.com>
2022-07-19 11:30:03 +0600

    initial commit

---
+++ phitron/course_3_dsa/week_2/module_5/class_ex.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+class person {
+  char name[20];
+  int id;
+
+public:
+  void getdetails() {}
+};
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  person p1;
+
+  return 0;
+}
