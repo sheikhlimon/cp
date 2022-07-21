69f2e4b879f86ed7030ebe88885eef43e14d072e
sheikhlimon <sheikhlimon404@gmail.com>
2022-07-21 13:46:43 +0600

    initial commit

--- phitron/course_3_dsa/week_2/module_6/test.cpp
+++ phitron/course_3_dsa/week_2/module_6/test.cpp
@@ -5,13 +5,8 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(0), cin.tie(0);
 
-  string s1 = "nortihP";
-  string:: reverse_iterator it;
-  for(it=s1.rbegin(); it!=s1.rend();it++)
-  {
-    cout<<*it;
-  }
-//   cout << s1.capacity()<<" "<<s1.size() << endl;
+  string s1 = "CGPA";
+  cout << s1.find('G');
 
   return 0;
 }
