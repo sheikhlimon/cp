e559c04b23fc49f0a30598e5643189ccb813bb9e
sheikhlimon <sheikhlimon404@gmail.com>
2022-07-26 13:36:56 +0600

    course_3/week_3/module_10

--- phitron/course_3_dsa/week_3/module_10/3_linear_sear.cpp
+++ phitron/course_3_dsa/week_3/module_10/3_linear_sear.cpp
@@ -11,25 +11,36 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        cin >>arr[i];
+        cin >> arr[i];
     }
-    int checkValue;
-    cout<<"enter the value for search: ";
-    cin>>checkValue;
 
-    int flag = 0;
-    
-    for(int i=0; i<n; i++)
+    char ch;
+    cout << "Do you want to search(Y/N): ";
+    cin >> ch;
+
+    while (toupper(ch) == 'Y')
     {
-        if(arr[i] == checkValue)
+        int checkValue;
+        cout << "enter the value for search: ";
+        cin >> checkValue;
+
+        int flag = 0;
+
+        for (int i = 0; i < n; i++)
         {
-            cout<<"Index No: "<< i << " Position: "<< i+1<<endl;
-            flag = 1;
+            if (arr[i] == checkValue)
+            {
+                cout << "Index No: " << i << " Position: " << i + 1 << endl;
+                flag = 1;
+            }
         }
-    }
-    if(flag == 0)
-    {
-        cout<<"not found";
+        if (flag == 0)
+        {
+            cout << "Not Found" << '\n';
+        }
+
+        cout << "Do you want to search again(Y/N): ";
+        cin >> ch;
     }
 
     return 0;
