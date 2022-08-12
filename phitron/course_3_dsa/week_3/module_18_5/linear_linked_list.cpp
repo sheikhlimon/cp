c014790d81ba33b6877afd82a970f111d84eb8b1
sheikhlimon <sheikhlimon404@gmail.com>
2022-08-12 12:12:27 +0600

    add insert at specific pos

--- phitron/course_3_dsa/week_3/module_18_5/linear_linked_list.cpp
+++ phitron/course_3_dsa/week_3/module_18_5/linear_linked_list.cpp
@@ -18,6 +18,7 @@ void insertAtHead(Node *&head, int val);
 void insertAtTail(Node *&head, int val);
 void display(Node *n);
 int countLength(Node *&head);
+void insertionAtSpecificPosition(Node *&head, int pos, int val);
 
 void insertAtHead(Node *&head, int val)
 {
@@ -70,14 +71,34 @@ int countLength(Node *&head)
     return count;
 }
 
+void insertionAtSpecificPosition(Node *&head, int pos, int val)
+{
+    int i = 0;
+    Node *temp = head;
+
+    while (i < pos - 2)
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    Node *newNode = new Node(val);
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
 int main()
 {
     Node *head = NULL;
 
     int value;
+    int pos;
+
     cout << "Choice 1: Insertion at Head"
          << endl
          << "Choice 2: Insertion at Tail"
+         << endl
+         << "Choice 3: Insertion at Specific Position"
          << endl
          << "Choice 0: Exit"
          << endl
@@ -88,16 +109,24 @@ int main()
     cin >> choice;
     while (choice != 0)
     {
-        cout << "Enter the value: ";
-        cin >> value;
         switch (choice)
         {
         case 1:
+            cout << "Enter the value: ";
+            cin >> value;
             insertAtTail(head, value);
             break;
         case 2:
+            cout << "Enter the value: ";
+            cin >> value;
             insertAtHead(head, value);
             break;
+        case 3:
+            cout << "Enter the Desired Postion: ";
+            cin >> pos;
+            cout << "Enter the value: ";
+            cin >> value;
+            insertionAtSpecificPosition(head, pos, value);
         default:
             break;
         }
