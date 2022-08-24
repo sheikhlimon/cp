af58bb96b434e1fb8d64a95fd6a56f095ec1f53d
sheikhlimon <sheikhlimon404@gmail.com>
2022-08-25 03:30:20 +0600

    initial commit

---
+++ phitron/course_3_dsa/week_7/stl_concep/test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v[0] = 100;
+    // v.push_back(100);
+    cout << v.size() << endl;
+    cout << v.capacity() << endl;
+    return 0;
+}
