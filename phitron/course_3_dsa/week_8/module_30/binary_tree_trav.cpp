8631a9fa90e3e46f0eac979a67f2c79e18068d49
sheikhlimon <sheikhlimon404@gmail.com>
2022-09-03 06:35:00 +0600

    initial commit

---
+++ phitron/course_3_dsa/week_8/module_30/binary_tree_trav.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class treeNode {
+  public:
+    int data;
+    treeNode *leftChild;
+    treeNode *rightChild;
+
+    treeNode(int value) {
+        data = value;
+        leftChild = NULL;
+        rightChild = NULL;
+    }
+};
+
+int main() {
+    cout << endl << endl << endl;
+    cout << endl << endl;
+    return 0;
+}
